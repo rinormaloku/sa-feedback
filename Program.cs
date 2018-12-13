a4957c94d76e940eb8d14a6560818cc4f2e2e761
rinormaloku <rinormaloku37@outlook.com>
2018-12-13 12:40:38 +0100

    start on the quad zero route

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace SA.Feedback
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:8080")
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     config.SetBasePath(Directory.GetCurrentDirectory());
